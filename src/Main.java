7a76028d0f0a7ce12a2ea129ac8605f4c017c7f4
Pawel Darulewski <pawel.darulewski@gmail.com>
2018-11-13 19:02:44 +0100

    Adding execution of all heuristic methods in Main

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,9 @@ public class Main {
         System.out.println(Arrays.deepToString(targetBoard.getPuzzleBoard()));
         new DFS(new Board(new PuzzleFileReader().readFile("puzzle.txt"), Board.INITIAL_BOARD), targetBoard).run();
         new BFS(new Board(new PuzzleFileReader().readFile("puzzle.txt"), Board.INITIAL_BOARD), targetBoard).run();
-        new BF(new Board(new PuzzleFileReader().readFile("puzzle.txt"), Board.INITIAL_BOARD), targetBoard, 1).run();
+//        new BF(new Board(new PuzzleFileReader().readFile("puzzle.txt"), Board.INITIAL_BOARD), targetBoard, 0).run(); // 0
+        new BF(new Board(new PuzzleFileReader().readFile("puzzle.txt"), Board.INITIAL_BOARD), targetBoard, 1).run(); // manhattan
+        new BF(new Board(new PuzzleFileReader().readFile("puzzle.txt"), Board.INITIAL_BOARD), targetBoard, 2).run(); // wrong placed
     }
 
 }
